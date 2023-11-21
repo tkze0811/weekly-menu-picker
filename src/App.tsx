b50c653c2f6efaa55f9d8f30064bc58b57c141c0
tkze0811 <149952231+tkze0811@users.noreply.github.com>
2023-11-21 15:03:44 +0900

    献立表の曜日毎の雛形を作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,45 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">ssssssssss</div>;
+  const weekdays: string[] = ["月", "火", "水", "木", "金"];
+
+  return (
+    <div className="App">
+      <h2>献立</h2>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            {weekdays.map((weekday, index) => {
+              return (
+                <th key={index} scope="col">
+                  {weekday}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">昼ご飯</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <th scope="row">夜ご飯</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
