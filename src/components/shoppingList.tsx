57892d6f9b1c935b99f6d6e36938288cf8206c94
tkze0811 <149952231+tkze0811@users.noreply.github.com>
2023-11-26 17:58:41 +0900

    買うものリストのスタイリング

--- src/components/shoppingList.tsx
+++ src/components/shoppingList.tsx
@@ -6,19 +6,29 @@ type Props = {
 };
 
 const ShoppingListContainer = styled.div`
+  padding: 20px 0;
   width: 300px;
   height: 500px;
+  text-align: center;
+`;
+const List = styled.ul`
+  margin-top: 20px;
+  padding-left: 40px;
+  padding-right: 20px;
+  width: 100%;
+  text-align: left;
+  box-sizing: border-box;
 `;
 
 export const ShoppingList = ({ className }: Props) => {
   return (
     <ShoppingListContainer className={className}>
       <h3>買うものリスト</h3>
-      <ul>
+      <List>
         <li>じゃがいも ×3</li>
         <li>バター ×3</li>
         <li>トマト ×3</li>
-      </ul>
+      </List>
     </ShoppingListContainer>
   );
 };
