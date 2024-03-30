1ced51aca9f04f6a9380341bee5b184caaa06fe6
tkze0811 <149952231+tkze0811@users.noreply.github.com>
2024-03-30 15:38:20 +0900

    firebase deployを出来るようにした

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./ress.min.css";
 import { App } from "./App";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 //reactの雛形を作っている
 const root = ReactDOM.createRoot(
